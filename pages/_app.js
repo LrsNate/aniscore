581932eadfc72c89beeae0290e97b0c36f16799b
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2021-05-23 16:47:00 +0200

    Add Google Tag

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,19 @@ import {
 } from "@material-ui/core";
 import { appWithTranslation } from "next-i18next";
 import { pdfjs } from "react-pdf";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const theme = createMuiTheme({});
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: "GTM-NSXSSPS",
+    });
+  });
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
