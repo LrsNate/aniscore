09e1240a4dae8f9679d4aa9a1385a4bb25d5ddef
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2021-05-24 00:39:15 +0200

    New score!

--- data/score.ts
+++ data/score.ts
@@ -60,6 +60,20 @@ const scores: Score[] = [
     instruments: [instruments.bbTrumpet, instruments.euphonium],
     difficulty: 2,
   },
+  {
+    id: "VtbZreuv",
+    datePublished: "2020-04-22",
+    title: {
+      "en-US": "Sound! Euphonium",
+      "ja-JP": "響け！ユーフォニアム",
+    },
+    origin: getShow("jxroO07P"),
+    slug: "hibike-euphonium",
+    pdfUrl: "/Hibike_Euphonium.pdf",
+    youtubeUrl: "https://www.youtube.com/embed/qPCRQisHpTQ",
+    instruments: [instruments.euphonium],
+    difficulty: 3,
+  },
 ];
 
 export function getScores(): Score[] {
