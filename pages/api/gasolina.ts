99bcb75973a578e817d32222cf361964d6de856a
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2021-06-02 22:12:16 +0200

    Fix the security validation

--- pages/api/gasolina.ts
+++ pages/api/gasolina.ts
@@ -6,18 +6,22 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   const PUBLIC_KEY =
     "fb9f6307ea0667a61fb3d9ea3a6ad001b5639c8bc5919c57a7dd25e759854896";
 
-  const signature = req.headers["X-Signature-Ed25519"] as string;
-  const timestamp = req.headers["X-Signature-Timestamp"] as string;
+  const signature = req.headers["x-signature-ed25519"] as string;
+  const timestamp = req.headers["x-signature-timestamp"] as string;
   const body = req.body;
 
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(timestamp + body),
-    Buffer.from(signature, "hex"),
-    Buffer.from(PUBLIC_KEY, "hex")
-  );
-
-  if (!isVerified) {
+  try {
+    const isVerified = nacl.sign.detached.verify(
+      Buffer.from(timestamp + JSON.stringify(body)),
+      Buffer.from(signature, "hex"),
+      Buffer.from(PUBLIC_KEY, "hex")
+    );
+    if (!isVerified) {
+      return res.status(401).end("invalid request signature");
+    }
+  } catch (e) {
     return res.status(401).end("invalid request signature");
   }
+
   res.status(200).json({ type: 1 });
 };
