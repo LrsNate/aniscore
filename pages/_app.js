2ff405f3a15a49f0ded65475b21cd1f58c671a45
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2021-07-14 22:23:33 +0200

    Remove Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,12 @@ import {
 } from "@material-ui/core";
 import { appWithTranslation } from "next-i18next";
 import { pdfjs } from "react-pdf";
-import ReactGA from "react-ga";
-import { useEffect } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const theme = createMuiTheme({});
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.initialize("UA-197723824-1");
-    console.log("Initialised tag manager");
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
